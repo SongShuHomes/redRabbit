731366dae746ac8994cf626e96b142298c1a83a3
songShu <1138197646@qq.com>
2022-11-26 19:21:44 +0800

    ✨ feat(首页): pc首页搭建

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,16 @@
   <div class="home">
     <img alt="Vue logo"
          src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    测试66
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
